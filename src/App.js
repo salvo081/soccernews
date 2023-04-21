0820a1dc82e3c817ce79e5c834333bb528a0ae76
Salvatore <111086457+salvo081@users.noreply.github.com>
2023-04-21 17:49:03 +0200

    display articles on page and search function implemented

--- src/App.js
+++ src/App.js
@@ -5,33 +5,51 @@ import {useState, useEffect} from "react";
 
 
 function App() {
-  const [news, setNews]= useState({});
+  const [news, setNews]= useState([]);
+  const [search, setSearch] = useState('');
+
   useEffect(() => {
+    // console.log(search);
+
+    const query = search !== '' ? search : 'football';
+
     fetch(
-      `http://hn.algolia.com/api/v1/search?query=football&tags=story`
+      `http://hn.algolia.com/api/v1/search?query=${query}&tags=story`
     )
       .then((res) => {
         if (!res.ok) throw new Error(`Didn't work because ${res.status}`);
-        console.log(res);
+        // console.log(res);
         return res.json();
       })
       .then((data) => {
-        console.log("data", data);
-        setNews(data);
+        // console.log("data", data.hits);
+        setNews(data.hits);
       })
       .catch((err) => console.log(err));
-  }, []);
+  }, [search]);
+
+  function setSearchValue(e) {
+    // console.log(e.target.value);
+    setSearch(e.target.value);
+  }
+
+  // console.log(news[0]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <input type="text" placeholder='Suche' id="suche"></input>
+        <input type="text" placeholder='Suche' id="suche" onChange={setSearchValue}></input>
       </header>
       <main className='App-main'>
         <h1>European Soccer News</h1>
-        <Article />
-        <Article />
-        <Article />
+        {/* funktioniert */}
+        {/* {console.log(news[0])} */}
+
+        {news.length && news.map((newsObj) => {
+          return <Article {...newsObj} />;
+        })}
+        
       </main>
       <footer className='App-footer'>
         <h1>Footer</h1>
